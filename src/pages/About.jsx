56e329368c2b6ceaff2f0057279e5b5b78080167
YeshwantRaoB <byeshwantrao@gmail.com>
2025-07-17 22:28:14 +0530

    fixed about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Lightbulb, Users, Target, Rocket } from "lucide-react";
+import { Lightbulb, Users, Target } from "lucide-react";
 
 const infoCards = [
   {
